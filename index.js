e67e78423b56b514e044c541eae69e5bef22965a
Salome Abigail <salomeabigail9@gmail.com>
2024-07-12 03:01:54 +0300

    created arrow functions file

---
+++ index.js
@@ -0,0 +1,17 @@
+// Function expression 'divide' that divides two numbers
+const divide = function(a, b) {
+    return a / b;
+  };
+  
+  // Arrow function 'square' that squares a number
+  const square = (x) => {
+    return x * x;
+  };
+  
+  // Arrow function 'add' that adds two numbers
+  const add = (a, b) => {
+    return a + b;
+  };
+
+
+
